7a59a5e0225c7543269788187a826b06fe0c4ece
DaddyWarcrimes <4444646+DaddyWarcrimes@users.noreply.github.com>
2023-02-15 20:19:46 -0600

    Vehicle data

---
+++ src/db/dataVehicle.sql
@@ -0,0 +1,6 @@
+INSERT INTO vehicle VALUES (1, 'Volvo', 'VNL', 2018, 'Truck', 20000,168,1 );
+INSERT INTO vehicle VALUES (2, 'Volvo', 'VNL', 2018, 'Truck', 20000,168,2 );
+INSERT INTO vehicle VALUES (3, 'Dodge', 'Ram', 1993, 'Van', 1500,73,3 );
+INSERT INTO vehicle VALUES (4, 'Volvo', 'VNL', 2018, 'Truck', 20000,168,4 );
+INSERT INTO vehicle VALUES (5, 'Volvo', 'VNL', 2018, 'Truck', 20000,168,5 );
+INSERT INTO vehicle VALUES (6, 'Ford', 'Pinto', 1973, 'Pinto', 1000,48,6 );
