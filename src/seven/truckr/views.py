529935cf1a8db5502587113546d16274d6eda5bf
DaddyWarcrimes <4444646+DaddyWarcrimes@users.noreply.github.com>
2023-03-25 00:55:34 -0500

    Removed obsolete code, added comment

--- src/seven/truckr/views.py
+++ src/seven/truckr/views.py
@@ -11,25 +11,21 @@ def employees(request):
     elist = employee.objects.order_by('employeeID')
     context = {'elist': elist}
     return(render(request, 'truckr/employees.html', context))
-    return HttpResponse("Employee page")
 
 def products(request):
     plist = product.objects.order_by('productID')
     context = {'plist': plist}
     return(render(request, 'truckr/products.html', context))
-    return HttpResponse("Product page")
 
 def purchaseOrders(request):
     polist  = purchaseOrder.objects.order_by('purchaseOrderID')
     context = {'polist':polist}
     return(render(request, 'truckr/purchaseOrders.html', context))
-    return HttpResponse("Purchase orders")
 
 def purchaseOrderDetail(request, ID):
-    #    items = orderItem.objects.raw('SELECT * from viewOrderProduct WHERE purchaseOrderID = %s', [ID])
+    # Join the orderitem table with the product table to display product data for that order. Filters for a specific purchase order
     items = orderItem.objects.raw('SELECT truckr_orderitem.id as id, truckr_orderitem.purchaseOrderID as purchaseOrderID, truckr_orderitem.productID as productID, truckr_product.productName as productName, truckr_product.price as price, truckr_orderitem.quantity as quantity, truckr_orderitem.status as status FROM truckr_orderitem INNER JOIN truckr_product ON truckr_orderitem.productID = truckr_product.productID WHERE truckr_orderitem.purchaseOrderID = %s', [ID])
-#    items = orderItem.objects.filter(purchaseOrderID = ID)
+#inferior code    items = orderItem.objects.filter(purchaseOrderID = ID)
     context = {'items':items}
-    #return HttpResponse("Viewing Purchase Order %s" % ID)
     return(render(request, 'truckr/purchaseOrdersDetail2.html', context))
 
