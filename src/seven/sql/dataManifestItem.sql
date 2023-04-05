94a69029f53459ead39952cf8c3c06fc9a57cf88
DaddyWarcrimes <4444646+DaddyWarcrimes@users.noreply.github.com>
2023-04-04 19:01:46 -0500

    Added manifest and manifest item tables and data

---
+++ src/seven/sql/dataManifestItem.sql
@@ -0,0 +1,89 @@
+-- manifestID 1
+INSERT INTO truckr_manifestitem (manifestID, productID, quantity) VALUES
+(1, 1, 12),
+(1, 23, 25),
+(1, 54, 7),
+(1, 67, 19),
+(1, 81, 3),
+(1, 89, 8);
+
+-- manifestID 2
+INSERT INTO truckr_manifestitem (manifestID, productID, quantity) VALUES
+(2, 4, 18),
+(2, 31, 2),
+(2, 43, 11),
+(2, 57, 13),
+(2, 73, 5);
+
+-- manifestID 3
+INSERT INTO truckr_manifestitem (manifestID, productID, quantity) VALUES
+(3, 5, 14),
+(3, 22, 6),
+(3, 34, 9),
+(3, 51, 20),
+(3, 65, 7),
+(3, 83, 4),
+(3, 96, 3);
+
+-- manifestID 4
+INSERT INTO truckr_manifestitem (manifestID, productID, quantity) VALUES
+(4, 9, 7),
+(4, 27, 11),
+(4, 38, 16),
+(4, 47, 9),
+(4, 60, 3),
+(4, 79, 8),
+(4, 90, 12);
+
+-- manifestID 5
+INSERT INTO truckr_manifestitem (manifestID, productID, quantity) VALUES
+(5, 12, 20),
+(5, 28, 9),
+(5, 42, 5),
+(5, 53, 13),
+(5, 68, 8),
+(5, 82, 6);
+
+-- manifestID 6
+INSERT INTO truckr_manifestitem (manifestID, productID, quantity) VALUES
+(6, 18, 11),
+(6, 35, 8),
+(6, 49, 5),
+(6, 56, 14),
+(6, 72, 2),
+(6, 88, 9);
+
+-- manifestID 7
+INSERT INTO truckr_manifestitem (manifestID, productID, quantity) VALUES
+(7, 25, 16),
+(7, 37, 12),
+(7, 50, 8),
+(7, 63, 10),
+(7, 76, 5),
+(7, 91, 3);
+
+-- manifestID 8
+INSERT INTO truckr_manifestitem (manifestID, productID, quantity) VALUES
+(8, 7, 10),
+(8, 21, 14),
+(8, 39, 9),
+(8, 44, 11),
+(8, 59, 3),
+(8, 80, 6),
+(8, 95, 12);
+
+-- manifestID 9
+INSERT INTO truckr_manifestitem (manifestID, productID, quantity) VALUES
+(9, 16, 8),
+(9, 32, 5),
+(9, 45, 12),
+(9, 55, 7),
+(9, 71, 9),
+(9, 84, 6),
+(9, 97, 3);
+
+-- manifestID 10
+INSERT INTO truckr_manifestitem (manifestID, productID, quantity) VALUES
+(10, 2, 19),
+(10, 19, 7),
+(10, 33, 4);
